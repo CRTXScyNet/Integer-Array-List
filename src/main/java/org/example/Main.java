132e6c390f76f77fc4c58d750d4ddce13500f750
CRTXScyNet <CRTXScyNet@gmail.com>
2024-06-24 17:28:22 +0500

    Initial commit is lost. This is the final version.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        IntegerListImpl list = new IntegerListImpl();
-//        list.sortInsertion();
-//        IntegerListImpl list1 = new IntegerListImpl();
-//        list1.add(1);
-//        list1.add(4);
-//        list1.add(6);
-//        list1.add(23);
-//        list1.add(34);
-//
-//        list.add(23);
-//        list.add(4);
-//        list.add(1);
-//        list.add(34);
-//        list.add(6);
-//        IntegerListImpl.print(list);
-//        list.sortInsertion();
-//        IntegerListImpl.print(list);
-//        System.out.println(list1.equals(list));
-
-
         IntegerListImpl.checkTime(0);
         System.out.println();
         IntegerListImpl.checkTime(1);
